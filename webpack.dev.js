6f39e5aed96b9935478627047bf0f7b220653a46
berkeleycole <alyssamhope@gmail.com>
2019-07-15 20:03:59 -0700

    adds sass loaders

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,13 +7,16 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
-    stats: 'verbose',
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
