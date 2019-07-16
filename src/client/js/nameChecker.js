610ec831235d55d4fbf495006b9721949c04fa9e
berkeleycole <alyssamhope@gmail.com>
2019-07-15 18:55:11 -0700

    adds initial files

---
+++ src/client/js/nameChecker.js
@@ -0,0 +1,18 @@
+function checkForName(inputText) {
+    console.log("::: Running checkForName :::", inputText);
+    let names = [
+        "Picard",
+        "Janeway",
+        "Kirk",
+        "Archer",
+        "Georgiou"
+    ]
+
+    let x = 20 * (2 + 4;
+
+    if(names.includes(inputText)) {
+        alert("Welcome, Captain!")
+    }
+}
+
+export { checkForName }
