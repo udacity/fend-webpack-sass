5e6a78eb93be859a67cd59ea9d642611a2d40a00
berkeleycole <alyssamhope@gmail.com>
2019-07-17 18:06:53 -0700

    adds prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,15 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     module: {
         rules: [
             {
