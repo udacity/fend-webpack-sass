2b9f1831e1547528a824c3bcceb47fd9bd6913a2
Justin McCray <jwmccra1@asu.edu>
2020-03-01 10:55:32 -0700

    Added another fetch request

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -10,7 +10,13 @@ function handleSubmit(event) {
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
-    })
+    });
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip=85901&units=imperial&appid=f23da3f348ae6df2c02f6d8a80f4b888')
+    .then(res => res.json())
+    .then(function(res) {
+        console.log(res);
+        document.getElementById('results2').innerHTML = res.name
+    });
 }
 
 export { handleSubmit }
