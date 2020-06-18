9ba1d4174e2e40a25846f7ebcd61e3df29c894be
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-06-18 18:07:42 +0530

    Added .scss test case in module.exports in  webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,6 +11,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
