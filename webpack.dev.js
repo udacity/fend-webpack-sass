85dffa12dd8f4e40d775332782117c6c5c2e72ed
adrianage <64158136+adrianage@users.noreply.github.com>
2020-09-30 13:28:26 -0700

    Loaded the sass and css loaders

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,23 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
     module: {
-        rules: [
-            {
+        rules: [{
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
