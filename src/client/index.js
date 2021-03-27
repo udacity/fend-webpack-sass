3cfc2cd8aaed84ce16c1b07fd236ea029e3bb804
alilefta <alilefta95@gmail.com>
2021-03-27 20:57:22 +0300

    added sass loaders

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,12 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
+
 console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+(function (){
+
+})();
