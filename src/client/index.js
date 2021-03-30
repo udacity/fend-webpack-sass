7160e82e77a550af9a8af70cc141321c687fb949
alilefta <alilefta95@gmail.com>
2021-03-30 12:15:22 +0300

    Adding second API call

--- src/client/index.js
+++ src/client/index.js
@@ -7,6 +7,7 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
+
 export {
     checkForName,
     handleSubmit
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,5 @@
+const APIURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const APIKEY = '&appid=f20d62c24bafab987722d901dc74af75';
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -10,6 +12,13 @@ function handleSubmit(event) {
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
+    }).then(()=>{
+        fetch(`https://randomuser.me/api/`).then(res => res.json()).then(data=> {
+            console.log(data)
+            // const weather = document.createElement('h4');
+            // weather.innerHTML = data.main.temp;
+            // document.getElementById('results').appendChild(weather);
+        })
     })
 }
 
