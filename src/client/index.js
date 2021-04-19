1b33df09ddd54d65605ecf37142f7acaca15310a
Isra1997 <israragheb@gmail.com>
2021-04-19 22:43:09 +0200

    fixed:js functionality

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,7 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+export{
+    checkForName,
+    handleSubmit
+}
