ae3655d32dce6ad575b5bfc41e4e73096ca2a7f5
Faisal Mahboob <81893782+faisal-mahboob@users.noreply.github.com>
2021-08-13 10:31:28 -0400

    javascript changes
    made some updates so  javascript funcationalities

--- src/client/index.js
+++ src/client/index.js
@@ -11,3 +11,7 @@ console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+
+export {checkForName,
+       handleSubmit}
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,10 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     devtool: 'source-map',
     module: {
         rules: [
