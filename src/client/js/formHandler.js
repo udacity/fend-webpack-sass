5963b871bd1c3c1c59c966e52ecf4b8a798a00ab
Hanni Liebler <johanna.liebler@gmail.com>
2022-10-05 08:27:08 +0200

    update Client lib

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,19 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
 
-    Client.checkForName(formText)
+  Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
+  console.log("::: Form Submitted :::");
+  fetch("http://localhost:8081/test")
+    .then((res) => {
+      return res.json();
     })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
-    })
+    .then(function (data) {
+      document.getElementById("results").innerHTML = data.message;
+    });
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,31 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+  entry: "./src/client/index.js",
+  mode: "production",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
