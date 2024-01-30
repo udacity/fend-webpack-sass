11b904ac47dbedc927d89b48ad384457eef28b60
unknown <66J1867@gmail.com>
2024-01-30 19:53:04 +0400

    Implemented API Call Challenge

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -16,4 +16,59 @@ function handleSubmit(event) {
     })
 }
 
+
+
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
+btn.addEventListener('click', async function (event) {
+    event.preventDefault();
+    const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
+
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if (!res.ok) {
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await res.json();
+        const pokemonSprite = data.sprites.front_default;
+        const pokemon = document.getElementById('pokemon');
+        const type = document.getElementById('type');
+        const type_0 = data.types[0].type.name;
+        const type_1 = data.types[1] ? data.types[1].type.name : null;
+
+        result.innerHTML = `<img src="${pokemonSprite}" alt="Pokemon"/>`;
+        pokemon.innerHTML = pokemonName;
+        type.innerHTML = type_1 ? `${type_0} , ${type_1}` : type_0;
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+
 export { handleSubmit }
+
+
+
+
+
+/* const result = document.getElementById('result');
+const btn = document.getElementById('btn');
+btn.addEventListener('click', getRandomCat);
+
+function getRandomCat() {
+    fetch('http://placekitten.com/200/300')
+        .then(res => res.blob())  // Use .blob() for image data
+        .then(data => {
+            // Create a URL for the blob data and set it as the image source
+            const imageUrl = URL.createObjectURL(data);
+            result.innerHTML = `<img src="${imageUrl}" alt="Random Cat"/>`;
+        })
+        .catch(error => {
+            console.error('Error fetching cat:', error);
+        });
+} */
--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -5,3 +5,4 @@ let json = {
 }
 
 module.exports = json
+
